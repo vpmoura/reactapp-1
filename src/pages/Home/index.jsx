0e939817e87d18901ff6d3d59e5110f9e644eada
Vinicius Moura <vinicius-chabe@hotmail.com>
2022-09-01 17:26:00 -0300

    v-1.0.0

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export function Home() {
+  return (
+    <div className="container">
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder="DANIEL BUMBUM GULOSO" />
+      <button type="button">Adicionar</button>
+    </div>
+  );
+}
