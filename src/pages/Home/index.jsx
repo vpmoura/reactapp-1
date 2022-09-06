045dd06d44f93b5311f2e9d90a1ab6c222fc66da
Vinicius Moura <vinicius-chabe@hotmail.com>
2022-09-06 16:52:31 -0300

    v-1.0.3

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,19 @@
 import "./styles.css";
 
+import { Card } from "../../components/Card";
+
 export function Home() {
   return (
     <div className="container">
       <h1>Lista de presença</h1>
-      <input type="text" placeholder="DANIEL BUMBUM GULOSO" />
+      <input type="text" placeholder="Digite aqui o seu nome..." />
       <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
