c51ff6fe9cc07cdf46bd2a304b820f67fe29d9db
Vinicius Moura <vinicius-chabe@hotmail.com>
2022-09-06 23:47:29 -0300

    v-1.0.4

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,24 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 import { Card } from "../../components/Card";
 
 export function Home() {
+  const [studentName, setStudentName] = useState("");
+
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite aqui o seu nome..." />
+      <h1>Nome: {studentName}</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome"
+        onChange={(e) => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card name="Daniel Bumbum Guloso" time="10:55:25" />
+      <Card name="Sor Gostigui" time="11:00:10" />
+      <Card name="Henrique Gado" time="11:05:34" />
     </div>
   );
 }
